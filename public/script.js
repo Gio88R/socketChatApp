bb6e1fa60c1e287749307e39854ab8215584a3f9
Gio88R <giobyw@gmail.com>
2023-08-24 00:22:29 +0200

    Creating room and username work. Crash when joining/adding room

--- public/script.js
+++ public/script.js
@@ -1,20 +1,47 @@
-const socket = io({ autoConnect: false });
-const login = document.querySelector(".login");
-const btn = document.querySelector(".login button");
-const listOfMessages = document.querySelector("ul");
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
 
-const initChat = () => {
-    socket.connect();
+if (messageForm != null) {
+  const name = prompt('What is your name?')
+  appendMessage('You joined')
+  socket.emit('new-user', roomName, name)
 
-    const username = document.querySelector(".login input").value;
-    
-    socket.emit("user_connected", username);
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', roomName, message)
+    messageInput.value = ''
+  })
+}
 
-    socket.on("new_user_connected", (username) => {
-        const li = document.createElement("li")
-        li.innerText = username + " joined the room";
-        listOfMessages.appendChild(li);
-    });
-};  
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomLink.innerText = 'join'
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
 
-btn.addEventListener("click", initChat);
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
--- server.js
+++ server.js
@@ -1,23 +1,57 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-io.on("connection", (socket) => {
-    console.log("New user connected:", socket.id);
+const rooms = {};
 
-    socket.on("user_connected", (username) => {
-        io.emit("new_user_connected", username);
-    });
-
-    socket.on("disconnect", ( ) => {
-        console.log("A user disconnected");
-    });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-server.listen(3000, () => console.log("Server is up and running"));
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/');
+  }
+  rooms[req.body.room] = { users: {} };
+  res.redirect(req.body.room);
+  io.emit('room-created', req.body.room);
+});
+
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/');
+  }
+  res.sendFile(__dirname + '/public/room.html');
+});
+
+server.listen(3000);
+
+io.on('connection', socket => {
+  socket.on('new-user', (room, name) => {
+    socket.join(room);
+    rooms[room].users[socket.id] = name;
+    socket.to(room).broadcast.emit('user-connected', name);
+  });
+
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] });
+  });
+
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id]);
+      delete rooms[room].users[socket.id];
+    });
+  });
+});
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name);
+    return names;
+  }, []);
+}
