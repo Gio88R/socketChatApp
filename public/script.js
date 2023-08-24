d755c81b405b451f82acb0b381546f56d0c7aa94
sarasandahl <92533105+sarasandahl@users.noreply.github.com>
2023-08-24 14:58:01 +0200

    det sara suttit med

--- public/script.js
+++ public/script.js
@@ -1,20 +1,84 @@
 const socket = io({ autoConnect: false });
-const login = document.querySelector(".login");
-const btn = document.querySelector(".login button");
-const listOfMessages = document.querySelector("ul");
 
+//LOGIN
+const login = document.querySelector(".login")
+const btn = document.querySelector(".login button")
+const listOftMessages = document.querySelector(".login ul")
+
+//ROOMS
+// 1. skapar en variable som är kopplad till din knapp
+// 2. skapar du en event listener för en knapp 
+const createChatRoom = document.querySelector(".chatroom button")
+const listOfRooms = document.querySelector(".chatroom ul")
+
+//CHATT
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+const messageContainer = document.getElementById('message-container')
+
+//const name = prompt('What is your name?')
+//appendMessage('You joined');
 const initChat = () => {
+    //console.log("Starta Chatt");
     socket.connect();
 
+    //LOGIN
     const username = document.querySelector(".login input").value;
-    
-    socket.emit("user_connected", username);
-
+    //skicka eventet till servern, var noga att döpa till något så du vet vad som sker
+    //emit tar två argument, det första eventnamnet döper vi själva.andra event skickar vi med datan vi vill (sträng, usernamne, vad som)
+    socket.emit("send_username_to_server", username);
     socket.on("new_user_connected", (username) => {
         const li = document.createElement("li")
         li.innerText = username + " joined the room";
         listOfMessages.appendChild(li);
     });
-};  
 
+    socket.on("send_username_to_all", (username) => {
+        const li = document.createElement("li");
+        li.innerText = username + " joined the chat";
+        listOftMessages.appendChild(li);
+    });
+
+    //ROOMS 
+    socket.on("list_of_rooms", (rooms) => {
+        const li = document.createElement("li");
+        li.innerText = rooms;
+        listOfRooms.appendChild(li);
+        console.log(data);
+    });
+
+    //CHATT
+    socket.on('chat-message', (data) => {
+        appendMessage(`${data.username}: ${data.message}`);
+    });
+
+    //ta bort användare och meddela
+    socket.on('user_disconnected', (username) => {
+        const li = document.createElement("li");
+        li.innerText = username + " left the chat";
+        listOftMessages.appendChild(li);
+    });
+};
+
+//LOGIN
 btn.addEventListener("click", initChat);
+
+//ROOMS
+createChatRoom.addEventListener("click", () => {
+    const roomname = document.querySelector(".chatroom input").value;
+    socket.emit("join_room", roomname);
+});
+  
+//CHATT
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
+});
+
+function appendMessage(message) {
+    const messageElement = document.createElement( 'div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+};
--- server.js
+++ server.js
@@ -8,16 +8,54 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-    console.log("New user connected:", socket.id);
+const usernames = {}
 
-    socket.on("user_connected", (username) => {
-        io.emit("new_user_connected", username);
+//on vill ha två argument, eventmanet och en funktion
+io.on("connection", (socket) => {
+
+    //LOGIN
+    console.log("New user connected: ", socket.id);
+
+    socket.on("send_username_to_server", (username) => {
+        //console.log(data);
+        usernames[socket.id] = username;
+        socket.broadcast.emit("send_username_to_all", username);
     });
 
-    socket.on("disconnect", ( ) => {
+    //disconnecta om man stänger ner rutan
+    socket.on("disconnect", () => {
         console.log("A user disconnected");
+        //skicka ut att den har disconnected
+        socket.broadcast.emit('user_disconnected', usernames[socket.id]);
+        //ta bort från objektet
+        delete usernames[socket.id];
+    });
+
+    //ROOMS
+    socket.on("join_room", (room) => {
+        socket.join(room);
+        const roomList = convertMapOfSetsToObjectOfArrays(io.sockets.adapter.rooms);
+        io.emit("list_of_rooms", roomList);
+        console.log(roomList);
+      });
+
+    //CHATT
+    socket.on('send-chat-message', message => {
+        console.log(message)
+        io.emit('chat-message', { message: message, username: usernames[socket.id] });
     });
 });
+
+//ROOMS
+function convertMapOfSetsToObjectOfArrays(mapOfSets) {
+    const objectOfArrays = {};
+  
+    for (const [key, set] of mapOfSets) {
+      objectOfArrays[key] = Array.from(set);
+    }
+  
+    return objectOfArrays;
+  }
+  
 
 server.listen(3000, () => console.log("Server is up and running"));
