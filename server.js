211cadad8166b33ef5cf26933fde0b692c70aabf
markusohlsson <ohlssonmarkus1@gmail.com>
2023-08-24 12:53:01 +0200

    No longer crashes

--- server.js
+++ server.js
@@ -1,49 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 const rooms = {};
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post('/room', (req, res) => {
+app.post("/room", (req, res) => {
   if (rooms[req.body.room] != null) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
+
   rooms[req.body.room] = { users: {} };
   res.redirect(req.body.room);
-  io.emit('room-created', req.body.room);
+  io.emit("room-created", req.body.room);
 });
 
-app.get('/:room', (req, res) => {
+app.get("/:room", (req, res) => {
   if (rooms[req.params.room] == null) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
-  res.sendFile(__dirname + '/public/room.html');
+  res.sendFile(__dirname + "/public/room.html");
 });
 
 server.listen(3000);
 
-io.on('connection', socket => {
-  socket.on('new-user', (room, name) => {
+io.on("connection", (socket) => {
+  socket.on("new-user", (room, name) => {
     socket.join(room);
+
+    if (!rooms[room]) {
+      rooms[room] = { users: {} };
+    }
+
     rooms[room].users[socket.id] = name;
-    socket.to(room).broadcast.emit('user-connected', name);
+    io.to(room).emit("user-connected", name);
   });
 
-  socket.on('send-chat-message', (room, message) => {
-    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] });
+  socket.on("send-chat-message", (room, message) => {
+    io.to(room).emit("chat-message", {
+      message: message,
+      name: rooms[room].users[socket.id],
+    });
   });
 
-  socket.on('disconnect', () => {
-    getUserRooms(socket).forEach(room => {
-      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id]);
+  socket.on("disconnect", () => {
+    getUserRooms(socket).forEach((room) => {
+      socket
+        .to(room)
+        .broadcast.emit("user-disconnected", rooms[room].users[socket.id]);
       delete rooms[room].users[socket.id];
     });
   });
